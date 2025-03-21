e98771091f7b5df1372ae1d0b2022257393e5135
axelker <axelker@outlook.fr>
2025-03-21 10:33:17 +0100

    fix: front unit test and upload coverage

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -2,12 +2,22 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {JokesService} from "./services/jokes.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule,
+          MatToolbarModule,
+          MatIconModule,
+          MatButtonModule,
+          MatCardModule,
+          MatDividerModule],
       providers: [JokesService]
     }).compileComponents();
   });
